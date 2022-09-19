3e15752e5b521ed2829ec1d494d095b65788e844
Geneustace Wicaksono <83039642+SentientPlatypus@users.noreply.github.com>
2022-09-19 08:47:44 -0400

    added neuron code

--- src/neuron.rs
+++ src/neuron.rs
@@ -1,3 +1,15 @@
+use crate::dendrite::Dendrite
+
 pub struct Neuron {
-    
+    n_id: usize,
+    n_value: i8,
+    dendrites: Vec<Dendrite>,
+    n_bias: f8,
+    n_delta:i8
+}
+
+impl Neuron {
+    fn set_dendrites(&self, n:usize) {
+        print("kill me")
+    }
 }
