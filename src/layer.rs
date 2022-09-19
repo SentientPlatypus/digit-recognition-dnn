f64a513bda32c4bacf5204ce82897dead0bae6de
Sentientplatypus <trexxxxxxxxxxy@gmail.com>
2022-09-18 22:04:20 -0400

    wrote layer methods

--- src/layer.rs
+++ src/layer.rs
@@ -2,5 +2,19 @@ use crate::neuron::Neuron;
 
 
 pub struct Layer {
-    neurons:Vec<Neuron>
+    neurons: Vec<Neuron>
+}
+
+impl Layer {
+    fn initialize(&self, n:i8) {
+
+    }
+
+    fn set_neuron(&mut self, neuron:Neuron, index:usize) {
+        self.neurons[index] = neuron;
+    }
+
+    fn get_neuron(&self, index:usize) -> &Neuron {
+        &self.neurons[index]
+    }
 }
