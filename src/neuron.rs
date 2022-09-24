0e22468d8d4df4475472b196346743c49224f481
Sentientplatypus <trexxxxxxxxxxy@gmail.com>
2022-09-23 22:45:26 -0400

    changes in converting to grayscale

--- src/neuron.rs
+++ src/neuron.rs
@@ -3,16 +3,13 @@ use crate::dendrite::Dendrite;
 pub struct Neuron {
     n_id: usize,
     n_value: i8,
-    dendrites: Vec<Dendrite>,
+    weights: Vec<f64>,
     n_bias: f32,
     n_delta:i8
 }
 
 impl Neuron {
-    fn set_dendrites(&self, n:usize) {
-        let d:Dendrite = Dendrite {
-            d_weight = 0.0,
-            d_points_to = 789789
-        }
+    fn set_weights(&mut self, n:usize, weight:f64) {
+        self.weights[n] = weight;
     }
 }
