b768bb540169507881a78c09223595ca40d16e2d
Sentientplatypus <trexxxxxxxxxxy@gmail.com>
2022-09-26 10:50:10 -0400

    fixed for loop

--- src/network.rs
+++ src/network.rs
@@ -27,12 +27,12 @@ impl Network {
     pub fn build(layer_sizes:Vec<usize>) -> Network {
         let layers:Vec<Layer> = Vec::new();
         let mut network:Network = Network { layers: layers };
-        (0..layer_sizes).for_each(|n:usize|
+        for size in layer_sizes
         {
             network.layers.push(
-                Layer::build(n)
+                Layer::build(size)
             )
-        });
+        }
         network
     }
 }
